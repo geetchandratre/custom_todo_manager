1c92e017c7ec2299d264aac2a01f59bf722f52e0
Geet Chandratre <geet_316@hotmail.com>
2011-09-26 12:05:41 +0530

    Created new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustomTodoManager::Application.config.secret_token = 'ac354e33f2dddb834b5f7e77d563017659c828d2b8b3ba08e850fc548aae7d94709df3e90549b6c8ff21e151216781b6b2e05cb31ce50efc8c8bf641395e30f9'
